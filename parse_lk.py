0568f12177043e2c940e54b5fc360bee6f2318fb
Marlesson <marlessonsa@gmail.com>
2018-07-01 14:32:54 -0300

    parse console lucask

---
+++ parse_lk.py
@@ -0,0 +1,114 @@
+from argparse import ArgumentParser
+import cv2
+import time
+from scipy import signal
+import numpy as np
+import cv2 as cv
+import math
+import random
+import sys
+from util import *
+
+
+if __name__ == '__main__':
+
+  parser = ArgumentParser()
+  parser.add_argument("-f", "--file", dest="filename",
+                      help="Video", metavar="FILE")
+
+  parser.add_argument("-c", "--maxCorners",
+                      dest="maxCorners", type=int, default=100)
+
+  parser.add_argument("-s", "--sizeWinlk",
+                      dest="sizeWinlk", type=int, default=10)
+
+  args = parser.parse_args()
+  print(args)
+
+  # VideoCapture and Save
+  cap   = cv2.VideoCapture(args.filename)
+
+  # Define the codec and create VideoWriter object
+  fourcc = cv2.VideoWriter_fourcc('F','M','P','4')
+  out    = cv2.VideoWriter('output.avi', fourcc, 20.0, (int(cap.get(3)), int(cap.get(4))))
+
+
+  # Take first frame and find corners in it
+  ret, old_frame = cap.read()
+  old_gray       = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
+
+  # parameter to get features track
+  feature_params = dict(maxCorners=args.maxCorners,
+                        qualityLevel=0.3,
+                        minDistance=7,
+                        blockSize=7)   
+  p0             = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
+  
+  # Create a mask image for drawing purposes
+  mask = np.zeros_like(old_frame)    
+  i    = 1
+
+  # Log pixels flow
+  log_flow = []
+
+  while True:
+      # readframe
+      ret, frame = cap.read()
+
+      if not ret:
+          break
+
+      # Resent FeaturesTrack
+      if i % 2 == 0:
+          p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
+
+      # Frame in Grayscale
+      frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+      # Optical Flow
+      p1, st = Lucas_Kanade(old_gray, frame_gray, p0, args.sizeWinlk)
+  
+      # Select good points
+      good_new = p1[st==1]
+      good_old = p0[st==1]
+
+      # draw the tracks
+      for i, (new,old) in enumerate(zip(good_new, good_old)):
+          a,b   = new.ravel()
+          c,d   = old.ravel()
+
+          x     = a-c
+          y     = b-d
+          angle = math.atan2(x, y)
+          size  = math.sqrt(x**2+y**2)
+          
+          log_flow.append([a, b, c, d, x, y, angle, size])
+          #print((c,d), " -> ", (a,b), " ", (x, y), " ", angle, " ", size)
+          # continue if diff between pixels positions is less 1
+          t = 1
+          if np.fabs(x) <= t and np.fabs(y) <= t:
+              continue
+          
+          mask  = cv2.line(mask, (a,b), (c,d), rad2rgb(angle), 2)
+          frame = cv2.circle(frame,(a,b), 3, rad2rgb(angle), -1)
+
+      img = cv2.add(frame, mask)
+      
+      # Show imagem
+      cv2.imshow('frame',img)
+
+      # write the flipped frame
+      out.write(img)
+
+      k = cv2.waitKey(30) & 0xff
+      if k == 27:
+          break
+
+      # Now update the previous frame and previous points
+      old_gray = frame_gray.copy()
+      p0       = good_new.reshape(-1, 1, 2)
+      i = i+1
+
+  out.release()
+  cap.release()
+  cv2.destroyAllWindows()
--- util.py
+++ util.py
@@ -1,37 +1,14 @@
-import numpy as np
-import cv2
 import time
 from scipy import signal
-
-# def draw_flow(img, flow, step=16):
-#     h, w = img.shape[:2]
-#     y, x = np.mgrid[step/2:h:step, step/2:w:step].reshape(2,-1)
-#     fx, fy = flow[y,x].T
-#     lines = np.vstack([x, y, x+fx, y+fy]).T.reshape(-1, 2, 2)
-#     lines = np.int32(lines + 0.5)
-#     vis = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-    
-#     cv2.polylines(vis, lines, 0, (0, 255, 0))
-#     for (x1, y1), (x2, y2) in lines:      
-#         cv2.circle(vis, (x1, y1), 1, (0, 255, 0), -1)
-#     return vis
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy import linalg as LA
-import cv2 as cv
 import math
-import matplotlib.cm as cm
 from scipy.signal import convolve2d
 import random
-
+import sys
 
 def Lucas_Kanade(I1, I2, features, k = 1):
-    #oldframe = cv.imread(image1)
-    #I1 = cv.cvtColor(oldframe, cv.COLOR_BGR2GRAY)
-
-    #newframe = I2#cv.imread(image2)
-    #I2 = cv.cvtColor(newframe, cv.COLOR_BGR2GRAY)
-
     Gx  = np.reshape(np.asarray([[-1, 1], [-1, 1]]), (2, 2))  # for image 1 and image 2 in x direction
     Gy  = np.reshape(np.asarray([[-1, -1], [1, 1]]), (2, 2))  # for image 1 and image 2 in y direction
     Gt1 = np.reshape(np.asarray([[-1, -1], [-1, -1]]), (2, 2))  # for 1st image
@@ -101,67 +78,22 @@ def Lucas_Kanade(I1, I2, features, k = 1):
 
     return newFeature, status
 
-if __name__ == '__main__':
-    import sys
-    color = np.random.randint(0, 255, (500, 3))
+def rad2rgb(rad):
+    rgb = floatRgb(rad, -math.pi, math.pi)
+    return rgb
 
-    # parameter to get features
-    feature_params = dict(maxCorners=500,
-                          qualityLevel=0.3,
-                          minDistance=7,
-                          blockSize=7)    
-
-    cap   = cv2.VideoCapture('data/sample.flv')
-    #cap   = cv2.VideoCapture('data/slow.flv')
-
-    #cap = cv2.VideoCapture(0)
-    #cap.set(3,1280)
-    #cap.set(4,1024)
-
-    # Take first frame and find corners in it
-    ret, old_frame = cap.read()
-    old_gray       = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
-
-    #print(len(p0))
-    p0             = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
-
-    # Create a mask image for drawing purposes
-    mask = np.zeros_like(old_frame)    
-    while True:
-        
-        ret, frame = cap.read()
-        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        
-
-        # Optical Flow
-        p1, st = Lucas_Kanade(old_gray, frame_gray, p0, 7)
-    
-        # Select good points
-        good_new = p1[st==1]
-        good_old = p0[st==1]
-
-        # draw the tracks
-        for i, (new,old) in enumerate(zip(good_new, good_old)):
-            a,b   = new.ravel()
-            c,d   = old.ravel()
-            #t = 2
-            #if np.fabs(a - c) < t or np.fabs(b - d) < t:
-            #    continue
-
-            mask  = cv2.line(mask, (a,b), (c,d), color[i].tolist(), 2)
-            frame = cv2.circle(frame,(a,b),5, color[i].tolist(),-1)
-
-        img = cv2.add(frame, mask)
-        cv2.imshow('frame',img)
-
-        k = cv2.waitKey(30) & 0xff
-        if k == 27:
-            break
-
-        # Now update the previous frame and previous points
-        old_gray = frame_gray.copy()
-        p0       = good_new.reshape(-1,1,2)
-        print(len(p0))
-
-    cv2.destroyAllWindows()
-    cap.release()
+def floatRgb(mag, cmin, cmax):
+       """
+       Return a tuple of floats between 0 and 1 for the red, green and
+       blue amplitudes.
+       """
+       try:
+              # normalize to [0,1]
+              x = float(mag-cmin)/float(cmax-cmin)
+       except:
+              # cmax = cmin
+              x = 0.5
+       blue = min((max((4*(0.75-x), 0.)), 1.))*255
+       red  = min((max((4*(x-0.25), 0.)), 1.))*255
+       green= min((max((4*math.fabs(x-0.5)-1., 0.)), 1.))*255
+       return [red, green, blue]
